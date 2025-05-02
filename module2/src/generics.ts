5e68b78dda31421313644797b06d8fcf18ae6e36
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-02 21:48:31 +0600

    generics with type alias

--- module2/src/generics.ts
+++ module2/src/generics.ts
@@ -43,7 +43,31 @@
     // use it in object
 
     // const user: GenericsType2<object> = [
-    const user: GenericsType2<{ name: string, age: number }> = [
+    // const user: GenericsType2<{ name: string, age: number }> = [
+    //     {
+    //         name: 'nazmul',
+    //         age: 22,
+    //     },
+    //     {
+    //         name: 'akash',
+    //         age: 22,
+    //     }
+    // ]
+
+
+    type User = {
+        name: string,
+        age: number
+    }
+
+    interface User2 {
+        name: string,
+        age: number
+    }
+
+
+    // const user: GenericsType2<User> = [
+    const user: GenericsType2<User2> = [
         {
             name: 'nazmul',
             age: 22,
@@ -58,11 +82,11 @@
 
     // generic tuple
 
-    const uesr5: [string, string] = ['akash', 'batash']
+    const user5: [string, string] = ['akash', 'batash']
 
 
     type GenericTuple<X, Y> = [X, Y]
-    const uesr6: GenericTuple<string, string> = ['akash', 'batash']
+    const user6: GenericTuple<string, string> = ['akash', 'batash']
 
     const user7: GenericTuple<number, {name: string, age: number}> = [1234, {name: 'nazmul', age: 444}]
 
