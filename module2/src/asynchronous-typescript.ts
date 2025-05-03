2d6cf8cf4df6aa94afce67743010bdea715682aa
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-03 19:53:38 +0600

    asynchronous typescript

---
+++ module2/src/asynchronous-typescript.ts
@@ -0,0 +1,31 @@
+{
+ 
+    //promise
+
+    //simulate
+    const createPromise = () =>{
+        return new Promise((resolve, reject) =>{
+            const data: string = 'something'
+            if(data){
+                resolve(data)
+            }
+            else{
+                reject('data not found')
+            }
+        })
+    }
+
+    //calling createPromise
+    const showData =async() =>{
+        const data = await createPromise()
+        console.log(data);
+    }
+
+    showData()
+
+
+
+
+
+
+}
