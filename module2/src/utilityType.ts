6f1c3584942f26944418a27d53f8dda96126813e
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-03 22:16:07 +0600

    utility type - readonly

--- module2/src/utilityType.ts
+++ module2/src/utilityType.ts
@@ -11,16 +11,32 @@
     }
 
     type Name = Pick<Person, "name">
-    type NameAge = Pick<Person, "name" | "age">
+    type NameAge = Pick<Person, "name" | "age">      //pick name and age
 
 
 
     // omit
-    type ContactInfo = Omit<Person, 'name'| 'age'>
+    type ContactInfo = Omit<Person, 'name'| 'age'>     //omit name and age
 
 
-    //required
-    type PersonRequired = Required<Person>
+    //required                   
+    type PersonRequired = Required<Person>           //make all required
+
+
+    // readonly
+
+    type PersonReadOnly = Readonly<Person>
+
+    // const person1 = {
+    const person1 : PersonReadOnly = {
+        name: 'nazmul',
+        age: 22,
+        email: 'dsksdjk',
+        contactNo : 983789
+    }
+
+    person1.name = 'akash'
+
 
 
 
